0fd76c9d274f5cfe432592a7d2fa876c90a845c3
hugo.prigent <prigenthugo9@gmail.com>
2023-11-09 12:03:13 +0100

    commit3

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 Hugo PRIGENT, Louis VINCENT
 09/11/2023
 Main projet space invaders
+
 """
 #importation des librairies
 import tkinter as tk
@@ -14,6 +15,8 @@ window = tk.Tk()
 window.title("Space Invaders")
 window.geometry("1000x700")
 
+
+#pour les elements de la fentre on les organise en grid, en "tableau" au sens graphique
 game = tk.Canvas(window, width = 850, height = 650, bg = "black")
 game.grid(row = 2, column = 1, sticky = 'SW', padx = 5, pady = 5)
 
@@ -25,11 +28,11 @@ quit = tk.Button(window, text = "Quit game")
 quit.grid(row = 2, column = 2, pady = 200, padx = 30)
 
  
-score = tk.Label(window, text = "Score : ")
-score.grid(row = 1, column = 1)
+score = tk.Label(window, text = "Score : ", font  = ("Times New Roman", 15, "bold"))
+score.grid(row = 1, column = 1, sticky= "W")
 
-lives = tk.Label(window, text = "Live : ")
-lives.grid(row = 1, column = 2)
+lives = tk.Label(window, text = "Live : ", font = ("Times New Roman", 15, "bold"))
+lives.grid(row = 1, column = 1, sticky = "E")
 
 window.mainloop()
 
