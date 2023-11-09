1e92354e4a9a7dbdcefb79ac41b069fbffc816bb
hugo.prigent <prigenthugo9@gmail.com>
2023-11-09 11:38:16 +0100

    commit 2

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ Hugo PRIGENT, Louis VINCENT
 09/11/2023
 Main projet space invaders
 """
-
+#importation des librairies
 import tkinter as tk
+
+
+
+#creation de la fenetre: 
 
 window = tk.Tk()
 window.title("Space Invaders")
@@ -14,13 +18,13 @@ game = tk.Canvas(window, width = 850, height = 650, bg = "black")
 game.grid(row = 2, column = 1, sticky = 'SW', padx = 5, pady = 5)
 
 new_game = tk.Button(window, text = "New Game")
-new_game.grid(row = 2, column = 2, sticky ='N', pady = 200)
+new_game.grid(row = 2, column = 2, sticky ='N', pady = 200, padx = 30)
 
 
 quit = tk.Button(window, text = "Quit game")
-quit.grid(row = 2, column = 2, pady = 200)
+quit.grid(row = 2, column = 2, pady = 200, padx = 30)
 
-
+ 
 score = tk.Label(window, text = "Score : ")
 score.grid(row = 1, column = 1)
 
---
+++ wall.py
@@ -0,0 +1,23 @@
+"""
+Louis Vincent-Hugo Prigent
+9/11/2023
+script de l'objet mur
+
+"""
+
+
+class wall:
+    
+    # a l'initialisation, l'état du mur est complet, donc on remplit le tableau de tableau avec que des 1
+    def __init__(self): 
+        self.__state = [[1,1,1,1,1,1] , [1,1,1,1,1,1] , [1,1,1,1,1,1]]
+    
+
+    #fonction permettant de modifier le mur
+    #quand une case du mur est touchee, son etat passe a 0
+    def destroy(self, row, column):
+        self.__state[row][column] = 0
+        return (self.__state)
+
+
+    
