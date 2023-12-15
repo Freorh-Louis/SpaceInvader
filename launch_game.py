937932a3b877361e86d0b90406b002eb4e7603eb
Louis VINCENT <vincenlo.mail@gmail.com>
2023-12-15 14:39:57 +0100

    maj 15/12

--- launch_game.py
+++ launch_game.py
@@ -20,40 +20,14 @@ class Launch_game:
         
     
     def clear_window(self):
-        def all_children (self):
-            _list = self.__window.winfo_children()
-            for item in _list :
-                if item.winfo_children() :
-                    _list.extend(item.winfo_children())
-
-            return _list
-        widget_list = all_children(self)
-        for item in widget_list:
-            item.place_forget()
-        
-    
-    
-    def set_menu(self):
-        image_menu = tk.PhotoImage(file = 'images\menu2.png')
-
-        difficulty_disp = tk.Label(self.__window, text ="Choose your difficulty :")
-        canva_img = tk.Canvas(self.__window, width = 500, height = 700, bg = 'black' )
-
-        image1 = canva_img.create_image(250,350, image = image_menu)
-        background_label = tk.Label(image = image_menu)
-        background_label.place(x = 220, y = 0, relwidth = 1, relheight = 1)
-
-        difficulty = Launch_game(self.__window)
-        button_dif1 = tk.Button(self.__window, text = "Low", command =  difficulty.set_difficulty1)
-        button_dif2 = tk.Button(self.__window, text = "Medium", command = difficulty.set_difficulty2)
-        button_dif3 = tk.Button(self.__window, text = "Hard", command = difficulty.set_difficulty3)
-
-
-
-        difficulty_disp.place(x = 100, y = 0)
-        button_dif1.place(x = 100, y = 100)
-        button_dif2.place(x = 160, y = 100) 
-        button_dif3.place(x = 220 , y = 100)
+        widget_list = []
+        for e in self.__window.children:
+            widget_list.append(e)
+        for e in widget_list:
+            if self.__difficulty != 0:
+                self.__window.children[e].place_forget()
+            else: 
+                self.__window.children[e].grid_forget()
 
 
 
@@ -61,8 +35,9 @@ class Launch_game:
     #entrée : l'objet , sortie : valeur 1 attribuée pour la difficulté
     def set_difficulty1(self):
         self.__difficulty = 1
-        self.launch_game()
+        print(self.__difficulty)
         self.clear_window()
+        
     
     #cette méthode marche comme celle ci-dessus mais pour le niveau 2
     def set_difficulty2(self):
@@ -76,15 +51,33 @@ class Launch_game:
 
     
     def meth_new_game(self):
-        # ligne qui plante: self.game.destroy
-        self.set_menu()
+        self.__difficulty = 0
+        self.clear_window()
     
+
+    def launch_menu(self):
+        image_menu = tk.PhotoImage(file = 'images\menu2.png')
+
+        difficulty_disp = tk.Label(self.__window, text ="Choose your difficulty :")
+        canva_img = tk.Canvas(self.__window, width = 500, height = 700, bg = 'black' )
+
+        image1 = canva_img.create_image(250,350, image = image_menu)
+        background_label = tk.Label(image = image_menu)
+        background_label.place(x = 220, y = 0, relwidth = 1, relheight = 1)
+
+        button_dif1 = tk.Button(self.__window, text = "Low", command =  self.set_difficulty1)
+        button_dif2 = tk.Button(self.__window, text = "Medium", command = self.set_difficulty2)
+        button_dif3 = tk.Button(self.__window, text = "Hard", command = self.set_difficulty3)
+
+        difficulty_disp.place(x = 100, y = 0)
+        button_dif1.place(x = 100, y = 100)
+        button_dif2.place(x = 160, y = 100) 
+        button_dif3.place(x = 220 , y = 100)
+
+
     #méthode permettant de lancer le jeu, d'afficher le jeu de façon a ce qu'il soit dynamique
     #entrée : l'ojet, sortie : lancement et actualisation du jeu
     def launch_game(self):
-        
-        
-
         game = tk.Canvas(self.__window, width = 850, height = 650, bg = "black")
         game.grid(row = 2, column = 1, sticky = 'SW', padx = 5, pady = 5)
 
--- main.py
+++ main.py
@@ -23,26 +23,12 @@ window.geometry("1000x700")
 
 #page menu :
 
-image_menu = tk.PhotoImage(file = 'images\menu2.png')
+launcher = Launch_game(window)
 
-difficulty_disp = tk.Label(window, text ="Choose your difficulty :")
-canva_img = tk.Canvas(window, width = 500, height = 700, bg = 'black' )
-
-image1 = canva_img.create_image(250,350, image = image_menu)
-background_label = tk.Label(image = image_menu)
-background_label.place(x = 220, y = 0, relwidth = 1, relheight = 1)
-
-difficulty = Launch_game(window)
-button_dif1 = tk.Button(window, text = "Low", command =  difficulty.set_difficulty1)
-button_dif2 = tk.Button(window, text = "Medium", command = difficulty.set_difficulty2)
-button_dif3 = tk.Button(window, text = "Hard", command = difficulty.set_difficulty3)
-
-
-
-difficulty_disp.place(x = 100, y = 0)
-button_dif1.place(x = 100, y = 100)
-button_dif2.place(x = 160, y = 100) 
-button_dif3.place(x = 220 , y = 100)
+print(launcher.get_difficulty())
+launcher.launch_menu()
+launcher.launch_game()
+launcher.clear_window()
 
 
 
